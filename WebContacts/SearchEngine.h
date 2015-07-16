b891c65fcf184d21a269116b67bbeec70f93bb3b
Mohcine Belarrem <belarrem@idm.fr>
2015-07-16 07:23:39 +0200

    Added Search Engine

---
+++ WebContacts/SearchEngine.h
@@ -0,0 +1,13 @@
+//
+//  SearchEngine.h
+//  WebContacts
+//
+//  Created by mohcine  on 7/16/15.
+//  Copyright (c) 2015 mohcine . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchEngine : UIViewController
+
+@end
