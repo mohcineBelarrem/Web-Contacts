f66240c19093fa717424ce139bc67da74e8386af
Mohcine Belarrem <mbelarrem@elv.telecom-lille.fr>
2015-07-16 08:06:04 +0200

    Update SearchEngine.h
    Added Comments to SearchEngine.h

--- WebContacts/SearchEngine.h
+++ WebContacts/SearchEngine.h
@@ -6,6 +6,14 @@
 //  Copyright (c) 2015 mohcine . All rights reserved.
 //
 
+
+/*
+This is the controller that will take the search query and sends it to the Retriever
+//Comment added from gitHub
+*/
+
+
+
 #import <UIKit/UIKit.h>
 
 @interface SearchEngine : UIViewController
